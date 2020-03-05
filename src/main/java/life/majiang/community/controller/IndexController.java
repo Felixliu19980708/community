b602aade59bd8c2604c2b4eaf93992d33dcde221
Felixliu19980708 <1159450136@qq.com>
2020-03-05 18:03:50 +0800

    question

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,37 +1,24 @@
 package life.majiang.community.controller;
 
-import life.majiang.community.mapper.UserMapper;
-import life.majiang.community.model.User;
+import life.majiang.community.dto.PaginationDTO;
+import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
     @Autowired
-    private UserMapper userMapper;
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if(cookies == null)
-        {
-            return "index";
-        }
-        for(Cookie cookie : cookies)
-        {
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
-        return "index";}
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "7") Integer size) {
+        PaginationDTO pagination= questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
+        return "index";
+    }
 }
